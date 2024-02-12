b9689e8789f927c59ce5e40f9d69f5936041b48b
Austin McCallister <austinmccallister0@gmail.com>
2024-02-12 11:51:13 -0500

    Moved create_report

--- client/utils/util.py
+++ client/utils/util.py
@@ -3,6 +3,11 @@ import requests
 import os
 import inquirer
 from typing import Callable
+import sys
+import organization
+import partner
+import contacts
+from openpyxl import Workbook
 
 def search_function(query: str, searchList: list[str]) -> list[str]:
     """ Implements a search function using a search list and query.
@@ -58,3 +63,62 @@ def search_menu(filterList: list[str], callback: Callable) -> None:
         return
     filteredList = search_function(answer, filterList)
     callback(filteredList)
+
+def create_report() -> None:
+    """ Creates a full Excel report and exports to export.xlsx """
+    wb = Workbook()
+    ws = wb.active
+    orgdata = organization.get_all_data()
+    partnerdata = partner.get_all_data()
+    contactdata = contacts.get_all_data()
+
+    def get_contact(contact_id: int) -> dict:
+        """ Gets contact data for a certain partner_id using the contact data above 
+        
+        Parameters:
+        (int)contact_id: The id of the partner
+        
+        Returns:
+        (dict): The data from the db
+        
+        """
+        return list(filter(lambda x:x['id'] == contact_id, contactdata))[0]
+    
+    ws.append(['Type', 'Name', 'Description', 'Email', 'Address', 'Phone', "Expertise", 'Role', 'Type', 'URL'])
+    for org in orgdata:
+        orgcontact = get_contact(org['contact_fk'])
+        ws.append([
+            'Organization',
+            orgcontact['name'], 
+            org['description'], 
+            orgcontact['email'], 
+            orgcontact['address'], 
+            orgcontact['phone'], 
+            '', 
+            '', 
+            org['type'], 
+            org['url']
+        ])
+        for ptnr in filter(lambda x: x['organization_fk'] == org['id'], partnerdata):
+            partnercontact = get_contact(ptnr['contact_fk'])
+            ws.append([
+                'Partner', 
+                partnercontact['name'], 
+                ptnr['description'], 
+                partnercontact['email'], 
+                partnercontact['address'], 
+                partnercontact['phone'], 
+                ptnr['expertise'],
+                ptnr['role'],
+                '',
+                ''
+            ])
+        ws.append(['']*10)
+
+
+
+    try:
+        wb.save("export.xlsx")
+    except PermissionError:
+        input("Something went wrong when trying to access the export file. Please delete the file and try again.")
+        sys.exit(-1)
